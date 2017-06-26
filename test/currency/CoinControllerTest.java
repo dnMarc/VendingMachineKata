72d4d40884c8943ae27b2eb5fda49d9d14da4002
Dan Marcus <d.marcus.at.work@gmail.com>
2017-06-26 14:42:45 -0400

    REFACTOR-ADD Setup Method CoinControllerTest Class
    CREATE setup method in CoinControllerTest class.  All CoinController
        instances will be created with this JUnit @Before method call.

--- test/currency/CoinControllerTest.java
+++ test/currency/CoinControllerTest.java
@@ -1,22 +1,28 @@
 package currency;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import static currency.Coin.*;
 
 public class CoinControllerTest {
+    
+    private CoinController coinController;
+    
+    @Before
+    public void setup(){
+        coinController = new CoinController();
+    }
 
     @Test
     public void systemValueIsZeroCentsWithNoCoinsInserted() {
-        CoinController coinController = new CoinController();
         int systemBalanceInCents = coinController.getSystemBalanceInCents();
         assertEquals(0, systemBalanceInCents);
     }
     
     @Test
     public void systemValueIsFiveCentsAfterOneNickelInserted(){
-        CoinController coinController = new CoinController();
         coinController.insert(NICKEL);
         int systemBalanceInCents = coinController.getSystemBalanceInCents();
         assertEquals(5, systemBalanceInCents);
