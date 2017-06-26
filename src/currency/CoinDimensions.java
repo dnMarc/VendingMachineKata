634a4f79216cf79791f6d376f6705181ac7370c6
Dan Marcus <d.marcus.at.work@gmail.com>
2017-06-26 17:17:48 -0400

    REFACTOR-CREATE Constants w/Quarter Dimensions
    CREATE constants for Coin.QUARTER weight, diameter, and thickness in
        CoinDimensions.
    
    REPLACE magic numbers for QUARTER weight, diameter, and thickness
        in Coin.

--- src/currency/CoinDimensions.java
+++ src/currency/CoinDimensions.java
@@ -2,13 +2,17 @@ package currency;
 
 public class CoinDimensions {
 
-    public static final double NICKEL_WEIGHT_IN_GRAMS   = 5.000;
-    public static final double NICKEL_DIAMETER_IN_MM    = 21.21;
-    public static final double NICKEL_THICKNESS_IN_MM   = 1.95;
+    public static final double NICKEL_WEIGHT_IN_GRAMS       = 5.000;
+    public static final double NICKEL_DIAMETER_IN_MM        = 21.21;
+    public static final double NICKEL_THICKNESS_IN_MM       = 1.95;
     
-    public static final double DIME_WEIGHT_IN_GRAMS     = 2.268;
-    public static final double DIME_DIAMETER_IN_MM      = 17.91;
-    public static final double DIME_THICKNESS_IN_MM     = 1.35;
+    public static final double DIME_WEIGHT_IN_GRAMS         = 2.268;
+    public static final double DIME_DIAMETER_IN_MM          = 17.91;
+    public static final double DIME_THICKNESS_IN_MM         = 1.35;
+    
+    public static final double QUARTER_WEIGHT_IN_GRAMS      = 5.670;
+    public static final double QUARTER_DIAMETER_IN_MM       = 24.26;
+    public static final double QUARTER_THICKNESS_IN_MM      = 1.75;
     
     
 }
