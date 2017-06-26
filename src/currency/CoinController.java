ffff85db356b79ed799e5d32b09df25a8a0da293
Dan Marcus <d.marcus.at.work@gmail.com>
2017-06-26 15:02:36 -0400

    REFACTOR-CREATE Check If Coin Is A Nickel Method
    CREATE CoinController.coinIsANickel() to check if a Coin has the
        expected weight, diameter, and thickness of a NICKEL.  This method
        takes in a Coin as an argument, and executes the conditional checks
        previously housed in CoinController.insert().

--- src/currency/CoinController.java
+++ src/currency/CoinController.java
@@ -12,16 +12,23 @@ public class CoinController {
 
     public void insert(Coin ... insertedCoins) {
         for (Coin currentCoin : insertedCoins){
-            if ((currentCoin.weightInGrams == 5.000) &&
-                (currentCoin.diameterInMM  == 21.21) &&
-                (currentCoin.thicknessInMM == 1.95)){
-            
+            if (coinIsANickel(currentCoin)){
                 systemBalanceInCents += NICKEL_VALUE_IN_CENTS;
             }
             else{
                 systemBalanceInCents += DIME_VALUE_IN_CENTS;
             }
         }
+    }
+
+    private boolean coinIsANickel(Coin currentCoin) {
+        if ((currentCoin.weightInGrams == 5.000) &&
+            (currentCoin.diameterInMM  == 21.21) &&
+            (currentCoin.thicknessInMM == 1.95)){
+            
+            return true;
+        }
+        return false;
     }
 
     
