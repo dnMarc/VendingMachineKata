0e774eac2adf83b2b76d0f62bd451f154a4d7676
Dan Marcus <d.marcus.at.work@gmail.com>
2017-06-26 17:38:46 -0400

    PASS Test12-Display Insert Coin w/0 Coin Inserted
    INTEGRATION TEST - Ensure that VendingMachine.checkDisplay() returns
        the correct message based on the system balance maintained by a
        CoinController instance.
    
    CREATE VendingMachine.checkDisplay() that returns a call to
        VendingMachine.createSystemDisplay() with the current system
        balance passed as the argument.  The system balance is obtained
        by a call to CoinController.getSystemBalanceInCents().
    
    CREATE a CoinController instance in VendingMachine that can be called
        like described above.

--- test/combinedtests/IntegrationTests.java
+++ test/combinedtests/IntegrationTests.java
@@ -1,14 +1,20 @@
 package combinedtests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import vending.VendingMachine;
 
 public class IntegrationTests {
 
+    
+    //BEGIN Accept Coins Functionality Tests
+    
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void systemDisplaysInsertCoinWithNoCoinsInserted() {
+        VendingMachine vendingMachine = new VendingMachine();
+        String displayStatus = vendingMachine.checkDisplay();
+        assertEquals("INSERT COIN", displayStatus);
     }
 
 }
