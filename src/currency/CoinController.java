33fe6ca13bbb3b67c44a7da7dba32254bdf6845c
Dan Marcus <d.marcus.at.work@gmail.com>
2017-06-26 16:42:03 -0400

    REFACTOR-CREATE Coin Value Map In CoinController
    CREATE Map field in CoinController that stores all the valid Coin types
        as keys and their respective values (in cents) as the Map's values.
    
    CREATE an explicit constructor in CoinController, and have it call the
        Map initialization method described below.
    
    CREATE CoinController.initializeCoinValuesInCents() to initialize the
        Map of Coin values with the appropriate values for all three valid
        Coin types.

--- src/currency/CoinController.java
+++ src/currency/CoinController.java
@@ -1,6 +1,10 @@
 package currency;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import static currency.CoinDimensions.*;
+import static currency.Coin.*;
 
 public class CoinController {
     
@@ -8,7 +12,19 @@ public class CoinController {
     private static final int DIME_VALUE_IN_CENTS = 10;
     private static final int QUARTER_VALUE_IN_CENTS = 25;
     private int systemBalanceInCents = 0;
+    
+    private Map<Coin, Integer> coinValuesInCents = new HashMap<Coin, Integer>();
 
+    public CoinController(){
+        initializeCoinValuesInCents();
+    }
+
+    private void initializeCoinValuesInCents() {
+        coinValuesInCents.put(NICKEL, NICKEL_VALUE_IN_CENTS);
+        coinValuesInCents.put(DIME, DIME_VALUE_IN_CENTS);
+        coinValuesInCents.put(QUARTER, QUARTER_VALUE_IN_CENTS);
+    }
+    
     public int getSystemBalanceInCents() {
         return systemBalanceInCents;
     }
