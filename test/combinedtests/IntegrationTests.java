2c72f77c2c2806e8b4635005372665d341fd7861
Dan Marcus <d.marcus.at.work@gmail.com>
2017-06-26 17:47:19 -0400

    REFACTOR-ADD Setup Method IntegrationTests Class
    CREATE setup method in IntegrationTests class.  All VendingMachine
        instances will be created with this JUnit @Before method call.

--- test/combinedtests/IntegrationTests.java
+++ test/combinedtests/IntegrationTests.java
@@ -1,26 +1,31 @@
 package combinedtests;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import vending.VendingMachine;
 import static currency.Coin.*;
 
 public class IntegrationTests {
-
     
+    private VendingMachine vendingMachine;
+    
+    @Before
+    public void setup(){
+        vendingMachine = new VendingMachine();
+    }
+
     //BEGIN Accept Coins Functionality Tests
     
     @Test
     public void systemDisplaysInsertCoinWithNoCoinsInserted() {
-        VendingMachine vendingMachine = new VendingMachine();
         String displayStatus = vendingMachine.checkDisplay();
         assertEquals("INSERT COIN", displayStatus);
     }
     
     @Test
     public void systemDisplaysCurrentBalanceLessThanOneDollar(){
-        VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.insert(QUARTER, DIME);
         String displayStatus = vendingMachine.checkDisplay();
         assertEquals("$0.35", displayStatus);
