a7f6a233b109bf094f03cde6bedeaae72b684d22
Dan Marcus <d.marcus.at.work@gmail.com>
2017-06-26 13:59:35 -0400

    REFACTOR-ADD Setup Method VendingMachineTest Class
    CREATE setup method in VendingMachineTest class.  All VendingMachine
        instances will be created with this JUnit @Before method call.

--- test/vending/VendingMachineTest.java
+++ test/vending/VendingMachineTest.java
@@ -1,20 +1,26 @@
 package vending;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineTest {
+    
+    private VendingMachine vendingMachine;
+    
+    @Before
+    public void setup(){
+        vendingMachine = new VendingMachine();
+    }
 
     @Test
     public void displayInsertCoinWithZeroSystemValue() {
-        VendingMachine vendingMachine = new VendingMachine();
         String displayStatus = vendingMachine.createSystemDisplay(0);
         assertEquals("INSERT COIN", displayStatus);
     }
     
     @Test
     public void displaySystemBalanceLessThanTenCents(){
-        VendingMachine vendingMachine = new VendingMachine();
         String displayStatus = vendingMachine.createSystemDisplay(5);
         assertEquals("$0.05", displayStatus);
     }
